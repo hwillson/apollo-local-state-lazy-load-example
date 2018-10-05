a1cf162c6965d84c989f4934a59303326dd44f72
Hugh Willson <hugh@octonary.com>
2018-10-05 09:40:39 -0400

    Adjust to reflect that similar initializers are now only fired once

--- src/components/messages/initializers.js
+++ src/components/messages/initializers.js
@@ -1,21 +1,4 @@
-import gql from 'graphql-tag';
-
 export default {
-  messages: (cache) => {
-    const query = gql`
-      query GetMessages {
-        messages @client {
-          id
-        }
-      }
-    `;
-    try {
-      const { messages } = cache.readQuery({ query });
-      return messages.length ? null : [];
-    } catch (error) {
-      return [];
-    }
-  },
-
+  messages: () => [],
   readFilter: () => 'SHOW_ALL',
 };
